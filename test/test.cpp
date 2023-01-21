2d71686db96e560fd239aa8c0c831c60bb60e44f
Bodhi <1700580551@qq.com>
2023-01-21 09:09:58 +0800

    1

--- test/test.cpp
+++ test/test.cpp
@@ -1,11 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int R = 2e5 + 10;
+using ll = long long;
+struct
+{
+	int l, r, val, key, sz;
+	ll sum;
+	bool tag;
+} tree[R * 50];
+void split(int k, int sz, int &x, int &y)
+{
+}
+int merge(int x, int y)
+{
+}
+ll query(int l, int r)
+{
+	ll res = 0;
+
+	return res;
+}
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
-
+	int n, p, x, l, r, lastres = 0;
+	cin >> n;
+	for (int j = 1; j <= n; ++j)
+	{
+	}
 	return 0;
 }
