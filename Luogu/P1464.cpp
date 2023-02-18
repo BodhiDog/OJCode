ddbd49c650a11bf34ad8bdcb16b9aa903679d613
Bodhi <1700580551@qq.com>
2023-02-18 15:27:03 +0000

    add Luogu/P1464.cpp.
    Signed-off-by: Bodhi <1700580551@qq.com>

---
+++ Luogu/P1464.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+ll f[25][25][25];
+ll w(ll a, ll b, ll c)
+{
+	if (a <= 0 || b <= 0 || c <= 0)
+		return 1;
+	if (a > 20 || b > 20 || c > 20)
+		return w(20, 20, 20);
+	if (f[a][b][c])
+		return f[a][b][c];
+	if (a < b && b < c)
+		return f[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+	else
+		return f[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ll a, b, c;
+	while (true)
+	{
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1)
+			break;
+		printf("w(%lld, %lld, %lld) = %lld\n", a, b, c, w(a, b, c));
+	}
+	return 0;
+}
