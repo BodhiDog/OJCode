7fcaffe77a0f12a7b165618bc2a1eb32a506e8bc
Bodhi <1700580551@qq.com>
2023-04-09 13:27:18 +0000

    update Luogu/P4779※_Dijkstra的优先队列优化模板题.cpp.
    Signed-off-by: Bodhi <1700580551@qq.com>

--- Luogu/P4779※_Dijkstra的优先队列优化模板题.cpp
+++ Luogu/P4779※_Dijkstra的优先队列优化模板题.cpp
@@ -73,3 +73,75 @@ int main()
 	}
 	return 0;
 }
+
+// 2023.4.9重写版
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10, M = 2e5 + 10;
+struct Edge
+{
+	int to, len, next;
+} e[M];
+struct Q
+{
+	int x;
+	unsigned int len;
+	bool operator<(const Q &a) const { return len > a.len; }
+};
+int head[M], tot;
+unsigned int dis[N];
+bool b[N];
+void add(int x, int y, int l)
+{
+	e[tot] = {
+		.to = y,
+		.len = l,
+		.next = head[x]};
+	head[x] = tot;
+	++tot;
+}
+void dijkstra(int s)
+{
+	memset(dis, 0x3f, sizeof(dis));
+	dis[s] = 0;
+	priority_queue<Q> q;
+	q.push({s, 0});
+	int j, x, to, len;
+	while (!q.empty())
+	{
+		x = q.top().x;
+		q.pop();
+		if (!b[x]) // 测试发现只能将这句话写在外面
+		{
+			b[x] = true;
+			for (j = head[x]; ~j; j = e[j].next)
+			{
+				to = e[j].to, len = e[j].len;
+				if (dis[x] + len < dis[to])
+				{
+					dis[to] = dis[x] + (unsigned)len;
+					q.push({to, dis[to]});
+				}
+			}
+		}
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int j, n, m, s, x, y, l;
+	cin >> n >> m >> s;
+	memset(head, -1, sizeof(head));
+	for (j = 1; j <= m; ++j)
+	{
+		cin >> x >> y >> l;
+		add(x, y, l);
+	}
+	dijkstra(s);
+	for (j = 1; j <= n; ++j)
+		cout << dis[j] << ' ';
+	return 0;
+}
