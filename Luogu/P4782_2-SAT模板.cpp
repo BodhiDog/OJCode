4446c41621d020b66740125ac6d7c924f64a9c8e
Bodhi <haoxuan.pei@outlook.com>
2023-08-04 03:51:30 +0000

    add Luogu/P4782_2-SAT模板.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

---
+++ Luogu/P4782_2-SAT模板.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int R = 2e6 + 10;
+int dfn[R], low[R], tme, gcnt, group[R], sta[R], top;
+vector<int> son[R];
+void tarjan(int x)
+{
+	dfn[x] = low[x] = ++tme;
+	sta[++top] = x;
+	for (int to : son[x])
+	{
+		if (!dfn[to])
+		{
+			tarjan(to);
+			low[x] = min(low[x], low[to]);
+		}
+		else if (!group[to])
+		{
+			low[x] = min(low[x], dfn[to]);
+		}
+	}
+	if (low[x] == dfn[x])
+	{
+		++gcnt;
+		while (sta[top] != x)
+		{
+			group[sta[top--]] = gcnt;
+		}
+		group[sta[top--]] = gcnt;
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int n, m, x, a, y, b, j;
+	cin >> n >> m;
+	for (j = 1; j <= m; ++j)
+	{
+		cin >> x >> a >> y >> b;
+		son[x + n * a].push_back(y + n * (b ^ 1));
+		son[y + n * b].push_back(x + n * (a ^ 1));
+		// 假：点编号+n，真：不加
+	}
+	for (j = 1; j <= 2 * n; ++j)
+	{
+		if (!dfn[j])
+		{
+			tarjan(j);
+		}
+	}
+	for (j = 1; j <= n; ++j)
+	{
+		if (group[j] == group[j + n])
+		{
+			cout << "IMPOSSIBLE\n";
+			return 0;
+		}
+	}
+	cout << "POSSIBLE\n";
+	for (j = 1; j <= n; ++j)
+	{
+		cout << (group[j] < group[j + n]) << ' ';
+	}
+	return 0;
+}
