b1b9d7a3776675948886f3930ca4290dbeb31a48
Bodhi <haoxuan.pei@outlook.com>
2023-08-19 04:22:50 +0000

    update Luogu/P3381_最小费用最大流模板.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

--- Luogu/P3381_最小费用最大流模板.cpp
+++ Luogu/P3381_最小费用最大流模板.cpp
@@ -1,3 +1,107 @@
+// 1
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+const int N = 5e3 + 10, M = 5e4 + 10;
+struct
+{
+	int to, next;
+	ll cap, cost;
+} e[M * 2];
+int head[N], last[N], tot, s, t;
+ll dis[N], minc;
+bool b[N];
+void add(int x, int y, ll w, ll c)
+{
+	e[tot] = {y, head[x], w, c};
+	head[x] = tot++;
+}
+bool spfa()
+{
+	memset(dis, 0x3f, sizeof(dis));
+	memset(b, 0, sizeof(b));
+	dis[s] = 0;
+	queue<int> q;
+	q.push(s);
+	b[s] = false;
+	int x, to, i;
+	ll cost, cap;
+	while (!q.empty())
+	{
+		x = q.front();
+		q.pop();
+		b[x] = false;
+		for (i = head[x]; ~i; i = e[i].next)
+		{
+			to = e[i].to;
+			cap = e[i].cap;
+			cost = e[i].cost;
+			if (cap > 0 && dis[x] + cost < dis[to])
+			{
+				dis[to] = dis[x] + cost;
+				if (!b[to])
+				{
+					q.push(to);
+					b[to] = true;
+				}
+			}
+		}
+	}
+	return dis[t] != 0x3f3f3f3f3f3f3f3f;
+}
+ll dfs(int x, ll flow)
+{
+	if (x == t)
+	{
+		return flow;
+	}
+	b[x] = true;
+	int to, i;
+	ll cap, add, res = 0, cost;
+	for (i = last[x]; ~i; i = last[x] = e[i].next)
+	{
+		to = e[i].to;
+		cap = e[i].cap;
+		cost = e[i].cost;
+		if (!b[to] && cap > 0 && dis[to] == dis[x] + cost)
+		{
+			add = dfs(to, min(cap, flow));
+			e[i].cap -= add;
+			e[i ^ 1].cap += add;
+			flow -= add;
+			res += add;
+			minc += cost * add;
+		}
+	}
+	return res;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	memset(head, -1, sizeof(head));
+	int n, m, i, x, y;
+	ll w, c;
+	cin >> n >> m >> s >> t;
+	for (i = 1; i <= m; ++i)
+	{
+		cin >> x >> y >> w >> c;
+		add(x, y, w, c);
+		add(y, x, 0, -c);
+	}
+	ll maxf = 0;
+	while (spfa())
+	{
+		copy(head, head + N, last);
+		maxf += dfs(s, 1e10);
+	}
+	cout << maxf << ' ' << minc << '\n';
+	return 0;
+}
+
+// 2
 #include <bits/stdc++.h>
 using namespace std;
 
