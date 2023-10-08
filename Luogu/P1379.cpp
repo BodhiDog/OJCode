86e793e384e5ffa70768fe7817563a311c6e0124
Bodhi <haoxuan.pei@outlook.com>
2023-10-08 14:05:28 +0000

    update Luogu/P1379.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

--- Luogu/P1379.cpp
+++ Luogu/P1379.cpp
@@ -1,95 +1,95 @@
-// IDDFS（IDA*）写法1 
+// IDDFS（IDA*）写法1
 #include <bits/stdc++.h>
 using namespace std;
 
- #define reg register int
- const char r[5][5] = {
- 	"",
- 	" 123",
- 	" 804",
- 	" 765"};
- const int xx[5] = {0, 0, 1, 0, -1},
- 		  yy[5] = {0, 1, 0, -1, 0};
- char c[5][5];
- int maxdep, tot;
- inline int f()
- {
- 	int res = 0;
- 	reg i, j;
- 	for (i = 1; i <= 3; ++i)
- 	{
- 		for (j = 1; j <= 3; ++j)
- 		{
- 			res += r[i][j] != c[i][j];
- 		}
- 	}
- 	return res;
- }
- bool dfs(int x, int y, int step, int lastx, int lasty) // lastx和lasty是剪枝，防止跳过去又跳回来的情况
- {
- 	if (step == maxdep)
- 	{
- 		if (!f())
- 		{
- 			return true;
- 		}
- 		else
- 		{
- 			return false;
- 		}
- 	}
- 	reg j;
- 	int tx, ty;
- 	for (j = 1; j <= 4; ++j)
- 	{
- 		tx = x + xx[j];
- 		ty = y + yy[j];
- 		if (tx == lastx && ty == lasty)
- 		{
- 			continue;
- 		}
- 		if (c[tx][ty])
- 		{
- 			swap(c[x][y], c[tx][ty]);
- 			if (step + f() <= maxdep) // 另一种写法也可，详见云端测试数据
- 			{
- 				if (dfs(tx, ty, step + 1, x, y))
- 				{
- 					return true;
- 				}
- 			}
- 			swap(c[x][y], c[tx][ty]);
- 		}
- 	}
- 	return false;
- }
- int main()
- {
- 	reg i, j;
- 	int sx, sy;
- 	for (i = 1; i <= 3; ++i)
- 	{
- 		for (j = 1; j <= 3; ++j)
- 		{
- 			cin >> c[i][j];
- 			if (c[i][j] == '0')
- 			{
- 				sx = i;
- 				sy = j;
- 			}
- 		}
- 	}
- 	for (maxdep = 0;; ++maxdep)
- 	{
- 		if (dfs(sx, sy, 0, -1, -1)) // lx和ly先给一个不可能的值
- 		{
- 			break;
- 		}
- 	}
- 	cout << maxdep;
- 	return 0;
- }
- 
+#define reg register int
+const char r[5][5] = {
+	"",
+	" 123",
+	" 804",
+	" 765"};
+const int xx[5] = {0, 0, 1, 0, -1},
+		  yy[5] = {0, 1, 0, -1, 0};
+char c[5][5];
+int maxdep, tot;
+inline int f()
+{
+	int res = 0;
+	reg i, j;
+	for (i = 1; i <= 3; ++i)
+	{
+		for (j = 1; j <= 3; ++j)
+		{
+			res += r[i][j] != c[i][j];
+		}
+	}
+	return res;
+}
+bool dfs(int x, int y, int step, int lastx, int lasty) // lastx和lasty是剪枝，防止跳过去又跳回来的情况
+{
+	if (step == maxdep)
+	{
+		if (!f())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	reg j;
+	int tx, ty;
+	for (j = 1; j <= 4; ++j)
+	{
+		tx = x + xx[j];
+		ty = y + yy[j];
+		if (tx == lastx && ty == lasty)
+		{
+			continue;
+		}
+		if (c[tx][ty])
+		{
+			swap(c[x][y], c[tx][ty]);
+			if (step + f() <= maxdep) // 另一种写法也可，详见云端测试数据
+			{
+				if (dfs(tx, ty, step + 1, x, y))
+				{
+					return true;
+				}
+			}
+			swap(c[x][y], c[tx][ty]);
+		}
+	}
+	return false;
+}
+int main()
+{
+	reg i, j;
+	int sx, sy;
+	for (i = 1; i <= 3; ++i)
+	{
+		for (j = 1; j <= 3; ++j)
+		{
+			cin >> c[i][j];
+			if (c[i][j] == '0')
+			{
+				sx = i;
+				sy = j;
+			}
+		}
+	}
+	for (maxdep = 0;; ++maxdep)
+	{
+		if (dfs(sx, sy, 0, -1, -1)) // lx和ly先给一个不可能的值
+		{
+			break;
+		}
+	}
+	cout << maxdep;
+	return 0;
+}
+
 // IDA*写法2
 #include <bits/stdc++.h>
 using namespace std;
@@ -145,7 +145,7 @@ int main()
 		for (j = 1; j <= 3; ++j)
 		{
 			cin >> now[i][j];
-			if(now[i][j] == '0')
+			if (now[i][j] == '0')
 			{
 				x0 = i, y0 = j;
 			}
@@ -167,119 +167,119 @@ int main()
 }
 
 // 双向BFS
- #define check(x) x >= 1 && x <= 3
- using ll = long long;
- const char RES[5][5] = {
- 	"",
- 	" 123",
- 	" 804",
- 	" 765"};
- const int xx[5] = {0, 0, 1, 0, -1},
- 		  yy[5] = {0, 1, 0, -1, 0};
- struct Node
- {
- 	int step, x, y;
- 	char c[4][4];
- };
- queue<Node> q[2];				 // q[0]从前往后，q[1]从后往前
- unordered_map<ll, int> stepm[2]; // 将状态映射为步数
- unordered_map<ll, int> vism[2];	 // 将状态映射为是否访问
- inline int sqrtod(Node x)
- {
- 	ll res = 0;
- 	register int i, j;
- 	for (i = 1; i <= 3; ++i)
- 	{
- 		for (j = 1; j <= 3; ++j)
- 		{
- 			res = res * 10 + (x.c[i][j] - '0');
- 		}
- 	}
- 	return res;
- }
- inline bool expand(int a)
- {
- 	Node temp;
- 	temp = q[a].front();
- 	q[a].pop();
- 	ll t2;
- 	int tx, ty, x = temp.x, y = temp.y;
- 	for (register int j = 1; j <= 4; ++j)
- 	{
- 		tx = x + xx[j];
- 		ty = y + yy[j];
- 		if (check(tx) && check(ty))
- 		{
- 			swap(temp.c[x][y], temp.c[tx][ty]);
- 			t2 = sqrtod(temp);
- 			if (!vism[a][t2])
- 			{
- 				vism[a][t2] = true;
- 				++temp.step;
- 				temp.x = tx;
- 				temp.y = ty;
- 				stepm[a][t2] = temp.step;
- 				q[a].push(temp);
- 				--temp.step; // 回溯
- 				if (vism[!a][t2])
- 				{
- 					cout << stepm[!a][t2] + stepm[a][t2];
- 					return true;
- 				}
- 			}
- 			swap(temp.c[x][y], temp.c[tx][ty]);
- 		}
- 	}
- 	return false;
- }
- inline void bfs()
- {
- 	while (!q[0].empty())
- 	{
- 		if (expand(0))
- 		{
- 			return;
- 		}
- 		if (expand(1))
- 		{
- 			return;
- 		}
- 	}
- }
- int main()
- {
- 	ll t = 0;
- 	Node t1, t2;
- 	bool flag = true;
- 	register int i, j;
- 	for (i = 1; i <= 3; ++i)
- 	{
- 		for (j = 1; j <= 3; ++j)
- 		{
- 			cin >> t1.c[i][j];
- 			if (t1.c[i][j] == '0')
- 			{
- 				t1.x = i, t1.y = j;
- 			}
- 			t2.c[i][j] = RES[i][j];
- 			if (t1.c[i][j] != RES[i][j])
- 			{
- 				flag = false;
- 			}
- 			t = t * 10 + (t1.c[i][j] - '0');
- 		}
- 	}
- 	if (flag)
- 	{
- 		cout << '0';
- 		return 0;
- 	}
- 	t2.x = 2, t2.y = 2;
- 	vism[0][t] = true;
- 	stepm[0][t] = 0;
- 	t1.step = 0, t2.step = 0;
- 	q[0].push(t1);
- 	q[1].push(t2);
- 	bfs();
- 	return 0;
- }
+#define check(x) x >= 1 && x <= 3
+using ll = long long;
+const char RES[5][5] = {
+	"",
+	" 123",
+	" 804",
+	" 765"};
+const int xx[5] = {0, 0, 1, 0, -1},
+		  yy[5] = {0, 1, 0, -1, 0};
+struct Node
+{
+	int step, x, y;
+	char c[4][4];
+};
+queue<Node> q[2];				 // q[0]从前往后，q[1]从后往前
+unordered_map<ll, int> stepm[2]; // 将状态映射为步数
+unordered_map<ll, int> vism[2];	 // 将状态映射为是否访问
+inline int sqrtod(Node x)
+{
+	ll res = 0;
+	register int i, j;
+	for (i = 1; i <= 3; ++i)
+	{
+		for (j = 1; j <= 3; ++j)
+		{
+			res = res * 10 + (x.c[i][j] - '0');
+		}
+	}
+	return res;
+}
+inline bool expand(int a)
+{
+	Node temp;
+	temp = q[a].front();
+	q[a].pop();
+	ll t2;
+	int tx, ty, x = temp.x, y = temp.y;
+	for (register int j = 1; j <= 4; ++j)
+	{
+		tx = x + xx[j];
+		ty = y + yy[j];
+		if (check(tx) && check(ty))
+		{
+			swap(temp.c[x][y], temp.c[tx][ty]);
+			t2 = sqrtod(temp);
+			if (!vism[a][t2])
+			{
+				vism[a][t2] = true;
+				++temp.step;
+				temp.x = tx;
+				temp.y = ty;
+				stepm[a][t2] = temp.step;
+				q[a].push(temp);
+				--temp.step; // 回溯
+				if (vism[!a][t2])
+				{
+					cout << stepm[!a][t2] + stepm[a][t2];
+					return true;
+				}
+			}
+			swap(temp.c[x][y], temp.c[tx][ty]);
+		}
+	}
+	return false;
+}
+inline void bfs()
+{
+	while (!q[0].empty())
+	{
+		if (expand(0))
+		{
+			return;
+		}
+		if (expand(1))
+		{
+			return;
+		}
+	}
+}
+int main()
+{
+	ll t = 0;
+	Node t1, t2;
+	bool flag = true;
+	register int i, j;
+	for (i = 1; i <= 3; ++i)
+	{
+		for (j = 1; j <= 3; ++j)
+		{
+			cin >> t1.c[i][j];
+			if (t1.c[i][j] == '0')
+			{
+				t1.x = i, t1.y = j;
+			}
+			t2.c[i][j] = RES[i][j];
+			if (t1.c[i][j] != RES[i][j])
+			{
+				flag = false;
+			}
+			t = t * 10 + (t1.c[i][j] - '0');
+		}
+	}
+	if (flag)
+	{
+		cout << '0';
+		return 0;
+	}
+	t2.x = 2, t2.y = 2;
+	vism[0][t] = true;
+	stepm[0][t] = 0;
+	t1.step = 0, t2.step = 0;
+	q[0].push(t1);
+	q[1].push(t2);
+	bfs();
+	return 0;
+}
