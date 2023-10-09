f8f866a645680391f5f969d80b363145ac28aacb
Bodhi <haoxuan.pei@outlook.com>
2023-10-09 15:47:57 +0000

    update Luogu/P7910_多解.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

--- Luogu/P7910_多解.cpp
+++ Luogu/P7910_多解.cpp
@@ -1,4 +1,4 @@
-// 平衡树解法 
+// 平衡树解法
 #include <bits/stdc++.h>
 #include <bits/extc++.h>
 using namespace std;
@@ -16,7 +16,6 @@ int main()
 	for (i = 1; i <= n; ++i)
 	{
 		cin >> a[i];
-		
 	}
 	t.insert(make_pair(-1, -1));
 	char op;
@@ -38,74 +37,74 @@ int main()
 	return 0;
 }
 
-// 普通解法 
+// 普通解法
 #include <bits/stdc++.h>
 using namespace std;
- 
- int t[8001]; // t[]存储的是第i个元素在a[]中的原位置
- struct Node
- {
- 	int num;
- 	int subscr;
- } a[8001];
- bool cmp(Node a, Node b)
- {
- 	if (a.num != b.num)
- 	{
- 		return a.num < b.num;
- 	}
- 	else
- 	{
- 		return a.subscr < b.subscr;
- 	}
- 	// 不是普通的冒泡排序，而是如果元素相同就按照下标排序
- }
- int main()
- {
- 	int n, q, x, v, i = 0, j, p;
- 	short caozuo;
- 	cin >> n >> q;
- 	for (i = 1; i <= n; ++i)
- 	{
- 		cin >> a[i].num;
- 		a[i].subscr = i;
- 	}
- 	sort(a + 1, a + n + 1, cmp);
- 	for (i = 1; i <= n; ++i)
- 	{
- 		t[a[i].subscr] = i;
- 	}
- 	for (i = 1; i <= q; ++i)
- 	{
- 		cin >> caozuo;
- 		if (caozuo == 1)
- 		{
- 			cin >> x >> v;
- 			a[t[x]].num = v;
- 			for (j = 2; j <= n; ++j)
- 			{
- 				if (!cmp(a[j - 1], a[j]))
- 				{
- 					swap(a[j], a[j - 1]);
- 				}
- 			}
- 			for (j = n; j >= 2; --j)
- 			{
- 				if (!cmp(a[j - 1], a[j]))
- 				{
- 					swap(a[j], a[j - 1]);
- 				}
- 			}
- 			for (j = 1; j <= n; ++j)
- 			{
- 				t[a[j].subscr] = j;
- 			}
- 		}
- 		else // caozuo == 2
- 		{
- 			cin >> x;
- 			cout << t[x] << endl;
- 		}
- 	}
- 	return 0;
- }
+
+int t[8001]; // t[]存储的是第i个元素在a[]中的原位置
+struct Node
+{
+	int num;
+	int subscr;
+} a[8001];
+bool cmp(Node a, Node b)
+{
+	if (a.num != b.num)
+	{
+		return a.num < b.num;
+	}
+	else
+	{
+		return a.subscr < b.subscr;
+	}
+	// 不是普通的冒泡排序，而是如果元素相同就按照下标排序
+}
+int main()
+{
+	int n, q, x, v, i = 0, j, p;
+	short caozuo;
+	cin >> n >> q;
+	for (i = 1; i <= n; ++i)
+	{
+		cin >> a[i].num;
+		a[i].subscr = i;
+	}
+	sort(a + 1, a + n + 1, cmp);
+	for (i = 1; i <= n; ++i)
+	{
+		t[a[i].subscr] = i;
+	}
+	for (i = 1; i <= q; ++i)
+	{
+		cin >> caozuo;
+		if (caozuo == 1)
+		{
+			cin >> x >> v;
+			a[t[x]].num = v;
+			for (j = 2; j <= n; ++j)
+			{
+				if (!cmp(a[j - 1], a[j]))
+				{
+					swap(a[j], a[j - 1]);
+				}
+			}
+			for (j = n; j >= 2; --j)
+			{
+				if (!cmp(a[j - 1], a[j]))
+				{
+					swap(a[j], a[j - 1]);
+				}
+			}
+			for (j = 1; j <= n; ++j)
+			{
+				t[a[j].subscr] = j;
+			}
+		}
+		else // caozuo == 2
+		{
+			cin >> x;
+			cout << t[x] << endl;
+		}
+	}
+	return 0;
+}
