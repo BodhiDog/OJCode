9a5596e77bb48f373b35c4c4b6306aad2c84ea76
Bodhi <haoxuan.pei@outlook.com>
2023-10-11 11:11:14 +0000

    update Luogu/P4430.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

--- Luogu/P4430.cpp
+++ Luogu/P4430.cpp
@@ -3,20 +3,29 @@ using namespace std;
 
 using ll = long long;
 const int M = 9999991;
+int qpow(ll x, ll p)
+{
+	ll res = 1;
+	while (p)
+	{
+		if (p & 1)
+		{
+			res = res * x % M;
+		}
+		x = x * x % M;
+		p >>= 1;
+	}
+	return int(res);
+}
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
-	int n;
-	ll i;	
+	int n;	
 	cin >> n;
-	ll ans = 1;
-	for (i = 1; i <= n - 2; ++i) // n-2可能<0
-	{
-		ans = ans * n % M;
-	}
-	for (i = 1; i <= n - 1; ++i)
+	ll ans = qpow(n, n - 2);
+	for (ll i = 1; i <= n - 1; ++i)
 	{
 		ans = ans * i % M;
 	}
