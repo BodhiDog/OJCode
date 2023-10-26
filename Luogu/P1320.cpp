8cfc1d269a647312296efe68fa79b6e672ccafb9
Bodhi <haoxuan.pei@outlook.com>
2023-10-26 08:34:14 +0000

    add Luogu/P1320.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

---
+++ Luogu/P1320.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	string s, all;
+	int i, n = 0, cur = 0;
+	while (cin >> s)
+	{
+		++n;
+		all = all + s;
+	}
+	all = all + '.';
+	cout << n << ' ';
+	if (all[0] == '1') // 从0开始计数！！！
+	{
+		cout << "0 ";
+	}
+	for (i = 0; i < all.size() - 1; ++i)
+	{
+		++cur;
+		if (all[i] != all[i + 1])
+		{
+			cout << cur << ' ';
+			cur = 0;
+		}
+	}
+	cout << endl;
+	return 0;
+}
