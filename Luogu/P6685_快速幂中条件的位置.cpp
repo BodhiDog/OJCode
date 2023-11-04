2c673a00fd26f95b1b705274852142cd25870686
Bodhi <haoxuan.pei@outlook.com>
2023-11-04 13:35:35 +0000

    rename Luogu/P6685.cpp to Luogu/P6685_快速幂中条件的位置.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

--- Luogu/P6685_快速幂中条件的位置.cpp
+++ Luogu/P6685_快速幂中条件的位置.cpp
@@ -1,3 +1,33 @@
+// pow()
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int n, m, l = 1, r, mid;
+	cin >> n >> m;
+	r = n;
+	while (l <= r)
+	{
+		mid = (l + r) >> 1;
+		if (pow(mid, m) <= n)
+		{
+			l = mid + 1;
+		}
+		else
+		{
+			r = mid - 1;
+		}
+	}
+	cout << r << '\n';
+	return 0;
+}
+
+// 快速幂
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -21,7 +51,7 @@ bool check(ll x)
 				return false;
 			}
 		}
-		x *= x; // 可能乘完这一次会超，但同时p也会在下一次变为0，也就是无效的乘
+		x *= x; // 可能乘完这一次会超，但同时p也会在下一次变为0，也就是无效的乘，所以要在开头判断x>n
 		p >>= 1;
 	}
 	return res <= n;
