9c4459ccac8da6c4936166b7792381d9a39b5763
Bodhi <haoxuan.pei@outlook.com>
2023-11-04 13:29:34 +0000

    add Luogu/P6685.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

---
+++ Luogu/P6685.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+ll n, m;
+bool check(ll x)
+{
+	int p = m;
+	ll res = 1;
+	while (p)
+	{
+		if (x > n)
+		{
+			return false;
+		}
+		if (p & 1)
+		{
+			res *= x;
+			if (res > n)
+			{
+				return false;
+			}
+		}
+		x *= x;
+		p >>= 1;
+	}
+	return res <= n;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin >> n >> m;
+	int l = 1, r = n, mid;
+	while (l <= r)
+	{
+		mid = (l + r) >> 1;
+		if (check(mid))
+		{
+			l = mid + 1;
+		}
+		else
+		{
+			r = mid - 1;
+		}
+	}
+	cout << r << '\n';
+	return 0;
+}
