b5eb45712e4210e3b3aeb505d0234b5c699a067b
Bodhi <haoxuan.pei@outlook.com>
2024-02-11 13:58:31 +0000

    add Luogu/P4326.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

---
+++ Luogu/P4326.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	double r;
+	cin >> r;
+	cout << fixed << setprecision(6) << 3.14159265358979323 * r * r << '\n'
+		 << 2 * r * r << flush;
+	return 0;
+}
