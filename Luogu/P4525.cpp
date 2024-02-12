efa5d204bf1d60e11b88e84433dad09d1d302148
Bodhi <haoxuan.pei@outlook.com>
2024-02-12 10:12:27 +0000

    add Luogu/P4525.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

---
+++ Luogu/P4525.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double a, b, c, d, l, r;
+double F(double x) // a!=0
+{
+	if (a != 0 && c != 0)
+	{
+		return c * (a * x + b) / (a * a) + ((a * d - b * c) * log(abs(a * x + b)) / (a * a));
+	}
+	else if (a == 0 && c != 0)
+	{
+		return (c * x + d) * (c * x + d) / (2 * b * c);
+	}
+	else
+	{
+		return d * x / b;
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin >> a >> b >> c >> d >> l >> r;
+	if (a == 0)
+	{
+		cout << fixed << setprecision(6) << F(r) - F(l) << flush;
+	}
+	else
+	{
+		cout << fixed << setprecision(6) << F(r) - F(l) << flush;
+	}
+	return 0;
+}
