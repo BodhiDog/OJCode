6bb049158d22c8daaf835fe51b715874df1a2c93
Bodhi <haoxuan.pei@outlook.com>
2024-02-20 10:06:54 +0000

    update Luogu/P1429.cpp.
    Signed-off-by: Bodhi <haoxuan.pei@outlook.com>

--- Luogu/P1429.cpp
+++ Luogu/P1429.cpp
@@ -1,3 +1,150 @@
+// 直接建树然后判断（由于输入数据可能有重复点，所以要判重！！！）
+#include <bits/stdc++.h>
+using namespace std;
+
+const int R = 2e5 + 10;
+struct Point
+{
+	double pos[2];
+} a[R];
+struct
+{
+	int son[2], sz;
+	Point now;
+	double mn[2], mx[2];
+	bool notthis(const Point &oth) const
+	{
+		return now.pos[0] != oth.pos[0] || now.pos[1] != oth.pos[1];
+	}
+} t[R];
+#define lc(k) t[k].son[0]
+#define rc(k) t[k].son[1]
+#define sqr(x) ((x) * (x))
+map<pair<double, double>, bool> mp;
+int tot;
+double ans = DBL_MAX;
+double dis(Point a, Point b)
+{
+	return sqr(a.pos[0] - b.pos[0]) + sqr(a.pos[1] - b.pos[1]);
+}
+double guess(int k, Point p) // p在结点k管理的区间中可能取得的最小值
+{
+	double res = 0; // 如果p在矩形内，最小的距离可能是0
+	for (int i = 0; i < 2; ++i)
+	{
+		if (p.pos[i] > t[k].mx[i]) // （i=0为例）如果p在结点k管理区域更靠右，那么距离至少是到矩形边框的距离
+		{
+			res += sqr(p.pos[i] - t[k].mx[i]);
+		}
+		if (p.pos[i] < t[k].mn[i]) // 同上
+		{
+			res += sqr(t[k].mn[i] - p.pos[i]);
+		}
+	}
+	return res;
+}
+void pushup(int k)
+{
+	int ls = lc(k), rs = rc(k);
+	for (int i = 0; i < 2; ++i)
+	{
+		t[k].mn[i] = t[k].mx[i] = t[k].now.pos[i];
+		if (ls)
+		{
+			t[k].mn[i] = min(t[k].mn[i], t[ls].mn[i]);
+			t[k].mx[i] = max(t[k].mx[i], t[ls].mx[i]);
+		}
+		if (rs)
+		{
+			t[k].mn[i] = min(t[k].mn[i], t[rs].mn[i]);
+			t[k].mx[i] = max(t[k].mx[i], t[rs].mx[i]);
+		}
+	}
+}
+int build(int l, int r, int kd)
+{
+	if (l > r)
+	{
+		return 0;
+	}
+	int mid = (l + r) >> 1, k = ++tot;
+	nth_element(a + l, a + mid, a + r + 1, [&](Point &x, Point &y)
+				{ return x.pos[kd] < y.pos[kd]; });
+	t[k].now = a[mid];
+	lc(k) = build(l, mid - 1, kd ^ 1);
+	rc(k) = build(mid + 1, r, kd ^ 1);
+	pushup(k);
+	return k;
+}
+void query(int k, Point p)
+{
+	if (!k)
+	{
+		return;
+	}
+	if (t[k].notthis(p))
+	{
+		ans = min(ans, dis(t[k].now, p));
+	}
+	double lans = DBL_MAX, rans = DBL_MAX;
+	if (lc(k))
+	{
+		lans = guess(lc(k), p);
+	}
+	if (rc(k))
+	{
+		rans = guess(rc(k), p);
+	}
+	if (lans < rans)
+	{
+		if (lans < ans)
+		{
+			query(lc(k), p);
+		}
+		if (rans < ans)
+		{
+			query(rc(k), p);
+		}
+	}
+	else
+	{
+		if (rans < ans)
+		{
+			query(rc(k), p);
+		}
+		if (lans < ans)
+		{
+			query(lc(k), p);
+		}
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int n, i;
+	cin >> n;
+	for (i = 1; i <= n; ++i)
+	{
+		cin >> a[i].pos[0] >> a[i].pos[1];
+		if (mp[{a[i].pos[0], a[i].pos[1]}])
+		{
+			cout << "0.0000" << flush;
+			return 0;
+		}
+		mp[{a[i].pos[0], a[i].pos[1]}] = true;
+	}
+	build(1, n, 0);
+	for (i = 1; i <= n; ++i)
+	{
+		query(1, a[i]);
+	}
+	cout << fixed << setprecision(4) << sqrt(ans) << flush;
+	return 0;
+}
+
+// 一个个先检测，再插入 
 #include <bits/stdc++.h>
 using namespace std;
 
